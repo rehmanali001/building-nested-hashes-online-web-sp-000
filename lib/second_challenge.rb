729ee7849d0bfd32ea9a240587c54b3a7f104c73
Rehman Ali <rehmanali001@gmail.com>
2019-07-30 20:34:24 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,16 +1,11 @@
 def second_challenge
-  epic_tragedy = {
-    :montague => {
-      :patriacrh => {},
-      :matriarch => {},
-      :hero => {},
-      :hero_friends => []
-      }, 
-    :capulet => {
-      :patriacrh => {},
-      :matriarch => {},
-      :heroine => {},
-      :heroine_friends => []
-      }
-  }
-end
+  epic_tragedy = {	  epic_tragedy = {
+    :montague => {	    :montague => {     :patriarch => {}, :matriarch => {}, :hero => {}, :hero_friends => []
+      }, 	      },
+    :capulet => {	    :capulet => {
+      :patriarch => {}, :matriarch => {}, :heroine => {}, :heroine_friends => []
+      }}	      }}
+
+
+   	
+ end 	end
