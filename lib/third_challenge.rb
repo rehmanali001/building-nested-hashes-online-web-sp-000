08eeb76575a678639b96faaea48b73af05f7268a
Rehman Ali <rehmanali001@gmail.com>
2019-07-30 20:36:51 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,19 +2,19 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name => "Lord Montague", :age => "53"#your key/value pairs here
+        :name => "Lord Montague", :age => "53"
         },
       :matriarch => {
-        :name => "Lady Montague", :age => "54"#your key/value pairs here
+        :name => "Lady Montague", :age => "54"
         },
       :hero => {
-        :name => "Romeo", :age => "15", :status => "alive"#your key/value pairs here
+        :name => "Romeo", :age => "15", :status => "alive"
         },
       :hero_friends => []
    },
    :capulet => {
       :patriarch => {
-        :name => "Lord Capulet", :age => "50"#your key/value pairs here
+        :name => "Lord Capulet", :age => "50"
         },
       :matriarch => {
         :name => "Lady Capulet", :age => "51"
