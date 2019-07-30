7cf6c2c0e999562ba9ab24418bbf446008375f4c
Rehman Ali <rehmanali001@gmail.com>
2019-07-30 20:30:08 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -3,7 +3,7 @@ def second_challenge
     :montague => {
       :patriacrh => {:name => "Lord Montague", :age => "53"},
       :matriarch => {:name => "Lady Montague", :age => "54"},
-      :hero => {:name => "Romeo", :age "15"},
+      :hero => {:name => "Romeo", :age => "15"},
       :hero_friends => [{:name => "Benvolio", :age => "17", :attitude => "worried"}, {:name => "Mercutio", :age => "18", :attitude => "hot-headed"}]
       }, 
     :capulet => {
