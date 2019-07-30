d1b251733f0b9b9eda8ba70edced2f3dad422293
Rehman Ali <rehmanali001@gmail.com>
2019-07-30 20:15:37 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,8 +7,12 @@ def second_challenge
       :hero_friends => []
       }, 
     :capulet => {
-      #your key/value pairs here!
-      }}
+      :patriacrh => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+      }
+  }
 
   
 end
